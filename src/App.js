0f9ad301f535370be1845fe9a28bca5480532fa3
Olesia Timofeeva <olesia_timofeeva@epam.com>
2020-12-19 14:57:35 +0400

    create new App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./index.css";
+
+export default function App() {
+
+  
+  return (
+    <div className="App">
+      <Header/>
+      <h1>Movies App</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
